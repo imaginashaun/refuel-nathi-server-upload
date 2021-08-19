3c252e2996b75c7bb82457aece97c88ecfa1ca86
imaginashaun <idevelop4mobile@gmail.com>
2021-08-19 17:58:56 +0200

    Testing the MemberShipController.php

--- app/Http/Controllers/API/MemberShipController.php
+++ app/Http/Controllers/API/MemberShipController.php
@@ -11,13 +11,8 @@ class MemberShipController extends Controller
 
 
 
-        return response()->json([
-            "success"=>false,
-            "data"=>null,
-            "message"=>"Not a member"
-        ]);
 
-        $details=  DB::select("SELECT D.discount,P.price,P.status as payment_status,P.method,P.description, PO.product_id,O.user_id,O.created_at,DATE_ADD(O.created_at, INTERVAL 365 DAY) as expiry_date FROM `product_orders` PO INNER JOIN products PR ON  PR.id=PO.product_id INNER JOIN  Orders O On O.id=PO.order_id INNER JOIN payments P ON P.id=O.payment_id  CROSS JOIN discounts D  where O.user_id=? and PR.name='annual_subscription' and DATE_ADD(O.created_at, INTERVAL 365 DAY) > CURDATE() AND P.status='succeeded'
+        $details=  DB::select("SELECT D.discount,P.price,P.status as payment_status,P.method,P.description, PO.product_id,O.user_id,O.created_at,DATE_ADD(O.created_at, INTERVAL 365 DAY) as expiry_date FROM `product_orders` PO INNER JOIN products PR ON  PR.id=PO.product_id INNER JOIN  orders O On O.id=PO.order_id INNER JOIN payments P ON P.id=O.payment_id  CROSS JOIN discounts D  where O.user_id=? and PR.name='annual_subscription' and DATE_ADD(O.created_at, INTERVAL 365 DAY) > CURDATE() AND P.status='succeeded'
 ",[$id]);
 
 
