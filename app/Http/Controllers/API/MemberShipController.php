6b1aef44ccafdb14917c49cd6b467874f9413350
imaginashaun <idevelop4mobile@gmail.com>
2021-08-19 17:45:05 +0200

    Testing the MemberShipController.php

--- app/Http/Controllers/API/MemberShipController.php
+++ app/Http/Controllers/API/MemberShipController.php
@@ -9,8 +9,6 @@ class MemberShipController extends Controller
 {
     public function CheckMembership($id){
 
-        echo "FIRST";
-        die();
 
         $details=  DB::select("SELECT D.discount,P.price,P.status as payment_status,P.method,P.description, PO.product_id,O.user_id,O.created_at,DATE_ADD(O.created_at, INTERVAL 365 DAY) as expiry_date FROM `product_orders` PO INNER JOIN products PR ON  PR.id=PO.product_id INNER JOIN  Orders O On O.id=PO.order_id INNER JOIN payments P ON P.id=O.payment_id  CROSS JOIN discounts D  where O.user_id=? and PR.name='annual_subscription' and DATE_ADD(O.created_at, INTERVAL 365 DAY) > CURDATE() AND P.status='succeeded'
 ",[$id]);
